8e3ed4c1e1ae7fba87489736e89258eb0a791bef
Ghazalmdi <ghazal.mohammadi.telecom@gmail.com>
2025-01-01 20:03:38 +0330

    updated in 1/1/2025 at 20:03

--- Main/Book/models.py
+++ Main/Book/models.py
@@ -13,14 +13,19 @@ class Book(models.Model):
     is_available = models.BooleanField(default=True, verbose_name='در دسترس است')
     part_of_book = models.TextField(verbose_name='بخشی از کتاب', null=True, blank=True)
     about_book = models.TextField(verbose_name='درباره کتاب', null=True, blank=True)
-    publisher = models.ForeignKey('Publisher', models.PROTECT, 'publisher_books')
-    author = models.ForeignKey('Author', models.PROTECT, 'author_books')
+    publisher = models.ForeignKey('Publisher', models.PROTECT, 'publisher_books', verbose_name='ناشر')
+    author = models.ForeignKey('Author', models.PROTECT, 'author_books', verbose_name='نویسنده')
+    genre = models.ManyToManyField('Genre')
+    parent_category = models.ForeignKey('ParentCategory', models.PROTECT, 'parent_cat_book',
+                                        verbose_name='دسته بندی اصلی')
+    child_category = models.ManyToManyField('ChildCategory')
 
 
 class Publisher(models.Model):
     publisher_name = models.CharField(max_length=100)
     manager = models.CharField(max_length=50)
     about_publisher = models.TextField()
+    slug = models.SlugField(unique=True, allow_unicode=True, max_length=150, verbose_name='اسلاگ')
 
 
 class Author(models.Model):
@@ -35,5 +40,24 @@ class Author(models.Model):
     birthday = jmodel.jDateField(verbose_name='تاریخ تولد')
     from_country = models.CharField(choices=COUNTRIES, default='IRAN', verbose_name='از کشور', max_length=30)
     about_author = models.TextField(verbose_name='زندگینامه')
+    slug = models.SlugField(unique=True, allow_unicode=True, max_length=150, verbose_name='اسلاگ')
 
-# class 
+
+class Genre(models.Model):
+    genre_name = models.CharField(max_length=30, verbose_name='نام ژانر')
+    slug = models.SlugField(unique=True, allow_unicode=True, max_length=150, verbose_name='اسلاگ')
+
+
+class ParentCategory(models.Model):
+    parent_category_name = models.CharField(max_length=20, verbose_name='نام دسته بندی اصلی')
+    slug = models.SlugField(unique=True, allow_unicode=True, max_length=150, verbose_name='اسلاگ')
+    is_available = models.BooleanField(default=True, verbose_name='در دسترس')
+
+    # def __str__(self):
+    #     return self.parent_category_name
+
+
+class ChildCategory(models.Model):
+    parent = models.ForeignKey('ParentCategory', models.PROTECT, 'childToParentCategory', verbose_name='دسته بندی والد')
+    child_category_name = models.CharField(max_length=20, verbose_name='نام دسته ی فرزند')
+    is_available = models.BooleanField(default=True, verbose_name='در دسترس')
